b8c019d24de53f674367c4f077f31c7a2e7bca46
Jan Vanbuel <janvanbuel@hotmail.com>
2024-03-13 08:01:29 +0100

    update docs

--- examples/resources/atuin_user/resource.tf
+++ examples/resources/atuin_user/resource.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    atuin = {
-      source = "lightcone/atuin"
-    }
-  }
-}
-
-provider "atuin" {
-  # Configuration options
-}
-
-
 resource "atuin_user" "test" {
   username = "humfrey123"
   email    = "testing12345@yahoo.com"
---
+++ examples/resources/provider/provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    atuin = {
+      source = "lightcone/atuin"
+    }
+  }
+}
+
+provider "atuin" {
+  # Configuration options
+}
