d1a755573baeea594367a8760592d96be9557b05
Jan Vanbuel <janvanbuel@hotmail.com>
2024-03-13 17:11:35 +0100

    create random users

--- internal/provider/atuin_user_test.go
+++ internal/provider/atuin_user_test.go
@@ -2,19 +2,21 @@ package provider
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccExampleAtuinUserResource(t *testing.T) {
+	random_user := fmt.Sprintf("testUser%d", rand.Intn(1000))
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: testAccExampleAtuinUserResourceConfig("testUser523018", "pa$$word", "test1234@yahoo.com"),
+				Config: testAccExampleAtuinUserResourceConfig(random_user, "pa$$word", "test1234@yahoo.com"),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("atuin_user.test", "password", "pa$$word"),
 				),
@@ -23,7 +25,7 @@ func TestAccExampleAtuinUserResource(t *testing.T) {
 			{
 				ResourceName:  "atuin_user.test",
 				ImportState:   true,
-				ImportStateId: "testUser523018,pa$$word,indoor dish desk flag debris potato excuse depart ticket judge file exit",
+				ImportStateId: fmt.Sprintf("%s,pa$$word,indoor dish desk flag debris potato excuse depart ticket judge file exit", random_user),
 			},
 			// Update and Read testing
 			// {
